d2977292100a437906871136e03bd69f4fc732be
meet sathavare <meetsathavara10@gmail.com>
2024-08-23 10:51:47 +0530

    com

--- vite.config.js
+++ vite.config.js
@@ -1,50 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
 
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === 'production';
-
-  return {
-    plugins: [react()],
-    css: {
-      postcss: {
-        plugins: [tailwindcss, autoprefixer],
-      },
-      preprocessorOptions: {
-        scss: {},
-      },
-    },
-    build: {
-      outDir: 'dist', // Change output directory to 'dist'
-      rollupOptions: {
-        input: './src/index.jsx',
-        output: {
-          entryFileNames: 'main.js',
-        },
-      },
-      cssCodeSplit: isProduction,
-      minify: isProduction,
-      sourcemap: !isProduction,
-    },
-    server: {
-      open: true,
-    },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
-      extensions: ['.js', '.jsx', '.json', '.css', '.scss'],
-    },
-    esbuild: {
-      loader: 'jsx',
-      include: /src\/.*\.jsx?$/,
-      exclude: [],
-    },
-    optimizeDeps: {
-      include: ['@babel/preset-react'],
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  
 });
